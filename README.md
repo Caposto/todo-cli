ea73b6d5a09d3064df6345af07ee848605638c46
Christian Apostol <christiand.apostol@outlook.com>
2025-04-28 13:42:08 -0400

    simple ls using os/exec

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TODO CLI in Golang
+
+# Resources
+[Building an Interactive Shell in Golang](https://www.dolthub.com/blog/2023-03-29-interactive-shell-golang/)
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"log"
+)
 
 func main() {
 	fmt.Println("LS COMMAND")
+	ls := exec.Command("ls", ".")
+
+	output, err := ls.CombinedOutput()
+
+	if  err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(output))
 }
