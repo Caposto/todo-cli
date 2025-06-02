05f80ae3990097dae0af87f7d4a928799f7888cb
Christian Apostol <christiand.apostol@outlook.com>
2025-06-02 15:07:04 -0400

    enabled cd

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,6 +12,17 @@ import (
 func execInput(s string) error {
 	trimmedInput := strings.TrimSuffix(s, "\n")
 	args := strings.Split(trimmedInput, " ")
+
+	switch args[0] {
+		case "cd" :
+			if len(args) < 2 {
+				return errors.New("Path Required")
+			}
+			return os.Chdir(args[1])
+		case "exit":
+			os.Exit(0)
+	}
+
 	command := exec.Command(args[0], args[1:]...)
 	command.Stderr =  os.Stderr
 	command.Stdout = os.Stdout
@@ -21,7 +33,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// Create input loop
 	for {
-		fmt.Print(">")
+		fmt.Print("> ")
 		line, err := reader.ReadString('\n')
 
 		if err != nil {
